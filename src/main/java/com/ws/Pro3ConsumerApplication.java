fb28855d1ef507b5704dd26d97ed339253da88fb
wangshuai110228 <641882053@qq.com>
2019-02-22 11:23:10 +0800

    pro服务端框架

---
+++ src/main/java/com/ws/Pro3ConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource(value = {"classpath:dubbo-consumer.xml"})
+public class Pro3ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pro3ConsumerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ws/bean/Role.java
@@ -0,0 +1,36 @@
+package com.ws.bean;
+
+import java.io.Serializable;
+
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = -2506528584027834195L;
+    private Integer id;
+    private String rname;
+    private Integer info;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRname() {
+        return rname;
+    }
+
+    public void setRname(String rname) {
+        this.rname = rname;
+    }
+
+    public Integer getInfo() {
+        return info;
+    }
+
+    public void setInfo(Integer info) {
+        this.info = info;
+    }
+}
